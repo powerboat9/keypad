125ae367f8d0b2281e4f5bc227f1e6e7a32d92f6
powerboat9 <powerboat9@users.noreply.github.com>
2016-01-01 01:12:49 -0500

    Almost finished it.

--- keypad.lua
+++ keypad.lua
@@ -1,21 +1,50 @@
-local monitor = assert(peripheral.find("monitor"), "Could Not Find Keypad")
-local x, y = monitor.getSize()
-local xBuff, yBuff = ((x - 3) / 2), ((y - 3) / 2)
-
 monitor.setTextScale(1)
 monitor.clear()
+
+local monitor = assert(peripheral.find("monitor"), "Could Not Find Keypad")
+local x, y = monitor.getSize()
+if (floor(x / 2) * 2) == x then
+    x = x - 1
+end
+if (floor(y / 2) * 2) == y then
+    y = y - 1
+end
+local xBuff, yBuff = ((x - 3) / 2), ((y - 3) / 2)
 
 function writeRow(layer, txt)
     monitor.setCursorPos(xBuff + 1, yBuff + layer)
     monitor.write(txt)
 end
-writeRow(1, "123")
-writeRow(1, "456")
-writeRow(1, "789")
 
-local combo = {1, 2, 3, 4}
+function redraw()
+    monitor.clear()
+    writeRow(1, "123")
+    writeRow(2, "456")
+    writeRow(3, "789")
+end
+
+local combo = {1, 2, 3, 4} --The first nil value is the end of the array
 local comboPos = 1
 while true do
     event, clickX, clickY = os.pullEvent("monitor_touch")
+    local keyX, keyY = nil, nil
+    local pressed = nil
     if (clickX > xBuff) and (clickX < (xBuff + 4)) and (clickY > yBuff) and (clickY < (yBuff + 4)) then
-        --Do This
+        keyX, keyY = (clickX - xBuff), (clickY - yBuff)
+        pressed = keyX + ((keyY - 1) * 3)
+    end
+    if pressed and (pressed == combo[comboPos]) then
+        comboPos = comboPos + 1
+        if not combo[comboPos] then
+            break
+        end
+    elseif pressed then
+        comboPos = 1
+        monitor.clear()
+        writeRow(2, "!!!")
+        sleep(1)
+        redraw()
+    end
+end
+
+rs.
